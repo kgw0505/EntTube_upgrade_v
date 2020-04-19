af8ab774daae9200a68aa58c16a825842459c472
thoratica <admin@thoratica.net>
2020-04-19 22:12:50 +0900

    Update EntTube.js

--- EntTube.js
+++ EntTube.js
@@ -1,8 +1,24 @@
-let videoId = '', frameWidth = document.getElementById('entryCanvas').clientWidth, frameHeight = document.getElementById('entryCanvas').clientHeight
+let videoId = '',
+    frameWidth = document.getElementById('entryCanvas').clientWidth,
+    frameHeight = document.getElementById('entryCanvas').clientHeight,
+    engineWidth = document.getElementsByClassName('entryEngine')[0].clientWidth,
+    engineHeight = document.getElementsByClassName('entryEngine')[0].clientHeight,
+    child = document.createElement('div'),
+    logo = document.createElement('span')
 setInterval(() => {
-if (Entry.variableContainer.getVariableByName('EntTube').getValue() != 'entryCanvas' && Entry.variableContainer.getVariableByName('EntTube').getValue() != videoId) {
-videoId = Entry.variableContainer.getVariableByName('EntTube').getValue()
-document.getElementById('entryCanvasWrapper').innerHTML = `<iframe width="${frameWidth}" height="${frameHeight}" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
-console.log(`<iframe width="${frameWidth}" height="${frameHeight}" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
-}
+    if (Entry.variableContainer.getVariableByName('EntTube').getValue() != 'entryCanvas' && Entry.variableContainer.getVariableByName('EntTube').getValue() != videoId) {
+        document.getElementById('entryCanvas').style.display = 'none'
+        document.getElementsByClassName('entryMouseViewMinimize')[0].style.display = 'none'
+        videoId = Entry.variableContainer.getVariableByName('EntTube').getValue()
+        child.innerHTML = `<iframe id="youtube" width="${frameWidth}" height="${frameHeight}" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        logo.innerHTML = `<a id="logo" href="https://github.com/thoratica/EntTube" target="_blank" style="display: flex; align-items: center; justify-content: center; float: right;"><img style="height: ${frameHeight/15}px" src="https://github.com/thoratica/EntTube/raw/master/powered.png"></a>`
+        document.getElementById('entryCanvasWrapper').appendChild(child)
+        document.getElementsByClassName('entryEngine')[0].appendChild(logo)
+    } else if (Entry.variableContainer.getVariableByName('EntTube').getValue() == 'entryCanvas' && document.getElementById('entryCanvas').style.display == 'none') {
+        document.getElementById('entryCanvas').style.display = 'block'
+        document.getElementsByClassName('entryMouseViewMinimize')[0].style.display = 'block'
+        document.getElementById('youtube').remove()
+        document.getElementById('logo').remove()
+        videoId = Entry.variableContainer.getVariableByName('EntTube').getValue()
+    }
 }, 100)
